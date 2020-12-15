6550faa3d7ab561605da99a845ed453446183093
Bipu Bajgai <bipubajgai@gmail.com>
2020-12-15 10:49:15 +0800

    Redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+
+// const store = createStore()
+
+// ACTION -> Get Data increment/filter
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
+// REDUCER -> it checks the action, and based on action it modifies store
+// passed in initial state
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1;
+      break;
+    case 'DECREMENT':
+      return state - 1;
+      break;
+  }
+}
+
+// STORE -> GLobalised store
+let store = createStore(counter);
+
+// Display action in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH -> execute the action to reducer.
+store.dispatch(increment());
+store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
